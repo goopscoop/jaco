90f43eda898857f918b5ea4c202661227984f919
goopscoop <codybarrus@gmail.com>
2015-10-20 22:28:30 -0700

    river can start on any side and flow straight in any direction

--- playground/map.js
+++ playground/map.js
@@ -50,50 +50,99 @@ var createBlankMapCanvas = function(size){
 
 var createRiver = function( mapArr, tileSet, size ){
   // 0 = N, 1 = E, 2 = S, 3 = W
-  // var startingSide = randomNum(4);
-  var startingSide = 0;
-  if ( startingSide === 0 ) {
-    var toBuild = true;
-    var row = 0;
-    var col = randomNum(size);
-    mapArr[0].splice(col, 1, tileSet[3]);
-    while ( toBuild ) {
-      var direction = moveOneRow( mapArr, tileSet[3], row, col, size, 1 )
-      if ( direction.msg === 'fin' ){
-        toBuild = false;
-      } else {
-        if ( direction.dir === 'east' ) {
-          col ++;
-        } else if ( direction.dir === 'south' ){
-          row ++;
-        }
+  var startingSide = randomNum(4);
+  var init = initializeStartLocation(startingSide, size);
+  var row = init.row;
+  var col = init.col;
+  var toBuild = true;
+  var direction = 'south';
+  mapArr[row].splice(col, 1, tileSet[3]);
+
+  // itterates over the arrays, returning message 'fin' when it reaches an edge
+  // ending the river
+  while ( toBuild ) {
+    var hasMoved = moveOneRow( mapArr, tileSet[3], row, col, size, 0, direction )
+    console.log( hasMoved )
+    if ( hasMoved.msg === 'fin' ){
+      toBuild = false;
+    } else {
+      direction = hasMoved.dir
+      if ( hasMoved.dir === 'east' ) {
+        col++;
+      } else if ( hasMoved.dir === 'south' ){
+        console.log(row)
+        row++;
+      } else if ( hasMoved.dir === 'west' ){
+        col--;
+      } else if ( hasMoved.dir === 'north' ){
+        row--;
       }
     }
   }
 };
 
+var initializeStartLocation = function(startingSide, size){
+  if ( startingSide === 0 ) {
+    return { row: 0, col: randomNum( size ) };
+  } else if( startingSide === 1 ){
+    return { row: randomNum( size ), col: ( size - 1 ) };
+  } else if( startingSide === 2 ){
+    return { row: ( size - 1 ), col: randomNum(size) };
+  } else if( startingSide === 3 ){
+    return { row: randomNum(size), col: 0 };
+  }
+};
 
-var moveOneRow = function( mapArr, add, previousRow, previousCol, size, direction ){
-  if( previousRow !== (size - 1) && previousCol !== (size - 1) ){
-    if ( direction === 1 ){
-      console.log('right')
-      mapArr[previousRow].splice((previousCol + 1), 1, add);
-      return { msg: 'contiue', dir: 'east' };
-    }
-
-    if ( direction === 2 ){
-      console.log('down')
-      mapArr[(previousRow + 1)].splice(previousCol, 1, add );
-      return { msg: 'contiue', dir: 'south' };
-    }
+// takes the map array, tile to add, and other args, and based on the dirToMove adds
+// the tile to add to the appropriate place, returning an object with msg and the
+// direction moved. If cannot place element, returns an object with msg: fin signaling
+// that it's time to end the algorithm
+var moveOneRow = function( mapArr, add, previousRow, previousCol, size, dirToMove, direction ){
+  if ( dirToMove === 0 && !edgeOfMapReached( previousRow, previousCol, size, direction ).north() ){
+    mapArr[(previousRow - 1)].splice( previousCol, 1, add );
+    return { msg: 'contiue', dir: 'north' }
+  } else if ( dirToMove === 1 && !edgeOfMapReached( previousRow, previousCol, size, direction ).east() ){
+    mapArr[ previousRow ].splice(( previousCol + 1 ), 1, add );
+    return { msg: 'contiue', dir: 'east' };
+  } else if ( dirToMove === 2 && !edgeOfMapReached( previousRow, previousCol, size, direction ).south() ){
+    mapArr[( previousRow + 1 )].splice( previousCol, 1, add );
+    return { msg: 'contiue', dir: 'south' };
+  } else if ( dirToMove === 3 && !edgeOfMapReached( previousRow, previousCol, size, direction ).west() ){
+    mapArr[(previousRow)].splice(( previousCol - 1 ), 1, add );
+    return { msg: 'contiue', dir: 'west' };
   } else {
     return { msg: 'fin' };
   }
-}
+};
+
+var clearOfMapEdge = function( previousRow, previousCol, size, direction ){
+  return edgeOfMapReached( previousRow, previousCol, size, direction ).north()
+    && edgeOfMapReached( previousRow, previousCol, size, direction ).east()
+    && edgeOfMapReached( previousRow, previousCol, size, direction ).south()
+    && edgeOfMapReached( previousRow, previousCol, size, direction ).west()
+    ? true : false;
+};
+
+var edgeOfMapReached = function( previousRow, previousCol, size, direction ){
+  return {
+    north: function(){
+      return direction === 'north' && previousRow === 0 ? true : false;
+    },
+    east: function(){
+      return direction === 'east' && previousCol === ( size - 1 ) ? true : false;
+    },
+    south: function(){
+      return direction === 'south' && previousRow === ( size - 1 ) ? true : false;
+    },
+    west: function(){
+      return direction === 'west' && previousCol === 0 ? true : false;
+    }
+  }
+};
 
 var randomNum = function(ceil, floor){
   return Math.floor(Math.random() * ceil) + (floor ? floor : 0 );
-}
+};
 
 var drawMap = function(map){
   var mapElements = '<div>';
